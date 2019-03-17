69f6340428c384bf1d72786b64142be65e6a83b4
Exley McCormick <exleym@gmail.com>
2019-03-16 22:04:25 -0400

    move cache to module

--- flask_filter/base.py
+++ flask_filter/base.py
@@ -6,7 +6,9 @@ from typing import Union
 from flask_filter.schemas import FilterSchema
 
 
+
 class FlaskFilter(object):
+    __SCHEMA_MAP = {}
 
     def __init__(self, app: Flask = None):
         self.app = app
@@ -16,11 +18,10 @@ class FlaskFilter(object):
 
     def init_app(self, app: Flask):
         """Callback for initializing application """
-        app.extensions['filter'] = _FlaskFilterState(self)
         self.app = app
 
     def register_model(self, DbModel, ModelSchema):
-        self.app.extensions['filter'].update_state(DbModel, ModelSchema)
+        self.__SCHEMA_MAP[DbModel] = ModelSchema
 
     def search(self, DbModel: Model, filters: list,
                ModelSchema: Union[Schema, None] = None,
@@ -35,23 +36,9 @@ class FlaskFilter(object):
         return query.all()
 
     def _lookup_schema(self, DbModel):
-        model = self.app.extensions["filter"].get_schema(DbModel)
+        model = self.__SCHEMA_MAP.get(DbModel)
         if not model:
             raise TypeError('You must either map a schema to Model: {} using `register_model`'
                             'or pass a schema to the `ModelSchema` parameter'
                             'when calling `search`'.format(DbModel))
         return model
-
-
-class _FlaskFilterState(object):
-    """Remembers configuration for the (filter, app) tuple """
-
-    def __init__(self, filter: FlaskFilter):
-        self.filter = filter
-        self.schema_map = dict()
-
-    def update_state(self, DbModel: Model, ModelSchema: Schema):
-        self.schema_map[DbModel] = ModelSchema
-
-    def get_schema(self, DbModel: Model):
-        return self.schema_map.get(DbModel, None)

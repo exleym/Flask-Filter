55f2eb35825d9abc418a26e34f9ce91b5ee81c15
exley <exleym@gmail.com>
2019-10-24 19:18:18 -0400

    update FilterSchema for marshmallow 3.2

--- flask_filter/schemas.py
+++ flask_filter/schemas.py
@@ -23,7 +23,12 @@ class FilterSchema(Schema):
     value = fields.Field(required=True, allow_none=False)
 
     @post_load
-    def make_object(self, json):
+    def make_object(self, json, *args, **kwargs):
+        if kwargs.pop("many", False):
+            return [
+                self.make_object(j, *args, many=False, **kwargs)
+                for j in json
+            ]
         op = json.get("OP")
         field = json.get("field")
         value = json.get("value")

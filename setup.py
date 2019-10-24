b86d956f0e4f3442cf62c135d3efb77305a65bbb
exley <exleym@gmail.com>
2019-10-24 17:54:46 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ from setuptools import setup
 setup(
     name='Flask-Filter',
     version='0.1dev',
+    author="Exley McCormick",
+    author_email="exleym@gmail.com",
+    description="A Flask extension for creating standard resource searches",
     packages=['flask_filter',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/exleym/Flask-Filter",
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "marshmallow", "sqlalchemy", "Flask-SQLAlchemy"],
 )

0139b832ca29dd085afebd1b851ffa8c58b2b8eb
exley <exleym@gmail.com>
2019-10-24 18:07:23 -0400

    update setup to include nested packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='Flask-Filter',
@@ -7,11 +7,11 @@ setup(
     author="Exley McCormick",
     author_email="exleym@gmail.com",
     description="A Flask extension for creating standard resource searches",
-    packages=['flask_filter',],
+    packages=find_packages(exclude=["tests", "docs", "contrib"]),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/exleym/Flask-Filter",
+    url="https://github.com/exleym/Flask-Filter ",
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "marshmallow", "sqlalchemy", "Flask-SQLAlchemy"],
 )
